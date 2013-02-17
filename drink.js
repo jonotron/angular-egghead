1e8ab52dd9a68265b5affad5d1c8c08a722e33f0
Jonathan Bowers <jonotron@gmail.com>
2013-02-17 06:27:55 -0800

    Isolate scope @

---
+++ drink.js
@@ -0,0 +1,17 @@
+var app = angular.module("drinkApp", []);
+
+app.controller("AppCtrl", function($scope) {
+  $scope.ctrlFlavour = "blackberry";
+
+});
+
+app.directive("drink", function() {
+  return {
+    scope: {
+      flavour: "@", 
+    },
+
+    template: '<div>{{flavour}}</div>'
+
+  }
+});
