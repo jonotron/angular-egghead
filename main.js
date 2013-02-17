5a16141896083c49a3ecca94fe9fcadc38960607
Jonathan Bowers <jonotron@gmail.com>
2013-02-16 16:36:20 -0800

    Changed the method to a filter

--- main.js
+++ main.js
@@ -2,6 +2,11 @@ var myApp = angular.module('myApp', []);
 myApp.factory('Data', function() {
   return {message: "I'm data from a service"};
 });
+myApp.filter('reverse', function() {
+  return function(text) {
+    return text.split("").reverse().join(""); 
+  }
+});
 
 function FirstController($scope, Data) {
   $scope.data = Data;
@@ -9,8 +14,4 @@ function FirstController($scope, Data) {
 
 function SecondController($scope, Data) {
   $scope.data = Data;
-
-  $scope.reversedMessage = function(message) {
-    return message.split("").reverse().join("");
-  }
 }
