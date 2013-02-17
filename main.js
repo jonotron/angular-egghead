f0ddd2f8d95fc6ddd788f4d621191c951b22470c
Jonathan Bowers <jonotron@gmail.com>
2013-02-16 17:03:14 -0800

    filters

--- main.js
+++ main.js
@@ -1,17 +1,73 @@
 var myApp = angular.module('myApp', []);
-myApp.factory('Data', function() {
-  return {message: "I'm data from a service"};
-});
-myApp.filter('reverse', function() {
-  return function(text) {
-    return text.split("").reverse().join(""); 
-  }
+myApp.factory('Avengers', function() {
+  var Avengers = {};
+
+  Avengers.cast = [
+    {
+      name: "Robert Downey Jr.",
+      character: "Tony Stark / Iron Man"
+    },
+    {
+      name: "Chris Evans",
+      character: "Steve Rogers / Captain America}"
+    },
+    {
+      name: "Mark Ruffalo",
+      character: "Bruce Banner / The Hulk}"
+    },
+    {
+      name: "Chris Hemsworth",
+      character: "Thor"
+    },
+    {
+      name: "Scarlett Johansson",
+      character: "Natasha Romanoff / Black Widow"
+    },
+    {
+      name: "Jeremy Renner",
+      character: "Clint Barton / Hawkeye"
+    },
+    {
+      name: "Tom Hiddleston",
+      character: "Loki"
+    },
+    {
+      name: "Clark Gregg",
+      character: "Agent Phil Coulson"
+    },
+    {
+      name: "Cobie Smulders",
+      character: "Agent Maria Hill"
+    },
+    {
+      name: "Stellan Skarsgard",
+      character: "Selvig"
+    },
+    {
+      name: "Samuel L. Jackson",
+      character: "Nick Fury"
+    },
+    {
+      name: "Gwyneth Paltrow",
+      character: "Pepper Potts"
+    },
+    {
+      name: "Paul Bettany",
+      character: "Jarvis (voice)"
+    },
+    {
+      name: "Alexis Denisof",
+      character: "The Other"
+    },
+    {
+      name: "Tina Benko",
+      character: "NASA Scientist"
+    }
+  ];
+
+  return Avengers;
 });
 
-function FirstController($scope, Data) {
-  $scope.data = Data;
-}
-
-function SecondController($scope, Data) {
-  $scope.data = Data;
+function AvengersController ($scope, Avengers) {
+  $scope.avengers = Avengers;
 }
