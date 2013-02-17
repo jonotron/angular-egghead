8999a28d6db636db4bcc0ad99ab3305765bda4b5
Jonathan Bowers <jonotron@gmail.com>
2013-02-17 07:03:53 -0800

    Review of isolate scopes

--- phone.js
+++ phone.js
@@ -1,8 +1,8 @@
 var app = angular.module('phoneApp', []);
 
 app.controller('AppCtrl', function($scope) {
-  $scope.callHome = function(message) {
-    alert(message); 
+  $scope.leaveVoicemail = function(number, message) {
+    console.log('Number: ' + number + ' said: ', message);
   }
 });
 
@@ -10,10 +10,18 @@ app.directive('phone', function() {
   return {
     restrict: "E", 
     scope: {
-      dial: '&'
+      number: '@',
+      network: '=',
+      makeCall: '&',
     },
 
-    template: '<input type="text" ng-model="value">' + 
-    '<div class="button" ng-click="dial({message:value})">Call home</div>'
+    template: '<div class="panel">Number: {{number}} Network:<select ng-model="network" ng-options="net for net in networks"></select>' +
+    '<input type="text" ng-model="value">' +
+    '<div class="button" ng-click="makeCall({number: number, message: value})">Call</div></div>',
+
+    link: function (scope) {
+      scope.networks = ["Rogers", "Telus", "Bell"];
+      scope.network = scope.networks[0];
+    }
   }
 })
