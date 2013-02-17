e7f8e4a0ece79fd2f1ea9287c63ec91f71842108
Jonathan Bowers <jonotron@gmail.com>
2013-02-17 06:18:22 -0800

    Isolate scope

---
+++ chore.js
@@ -0,0 +1,17 @@
+var app = angular.module('choreApp', []);
+
+app.controller("ChoreCtrl", function($scope) {
+  $scope.logChore = function (chore) {
+    alert(chore + ' is done'); 
+  }
+});
+
+app.directive('kid', function() {
+  return {
+    restrict: "E",
+    scope: {
+      done: "&" 
+    },
+    template: '<input type="text" ng-model="chore">{{ chore }} <div class="button" ng-click="done({chore:chore})">I am done</div>'
+  }
+})
