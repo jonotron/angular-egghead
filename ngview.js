50b6404f9917a67ccfe84ba22716f03f778a9b34
Jonathan Bowers <jonotron@gmail.com>
2013-03-02 14:16:37 -0800

    redirectTo

--- ngview.js
+++ ngview.js
@@ -2,17 +2,26 @@ var app = angular.module('app', []);
 
 app.config(function($routeProvider) {
   $routeProvider
-    .when('/map/:country/:state/:city', {
+    .when('/', {
       templateUrl: 'ngview.tmpl.html',
       controller: 'AppCtrl'
+    })
+    .when('/pizza/:crust/:toppings', {
+      redirectTo: function(routeParams, path, search) {
+        console.log(routeParams, path, search);
+        return '/' + routeParams.crust; 
+      }
+    })
+    .when('/deep', {
+      template: 'Deep dish' 
+    })
+    .otherwise({
+      redirectTo:'/'
     });
 });
 
 app.controller('AppCtrl', function($scope, $routeParams) {
   $scope.model = {
-    message: "Address: " + 
-      $routeParams.country + ', ' +
-      $routeParams.state + ', ' +
-      $routeParams.city
+    message: "This is my app!!" 
   }
 });
