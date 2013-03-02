90a685d1d27bd34568d12c88b9be092ec58ca332
Jonathan Bowers <jonotron@gmail.com>
2013-03-02 14:04:33 -0800

    api

--- ngview.js
+++ ngview.js
@@ -1,10 +1,17 @@
 var app = angular.module('app', []);
 
 app.config(function($routeProvider) {
-  $routeProvider.when('/', {
-    templateUrl: 'ngview.tmpl.html',
-    controller: 'AppCtrl'
-  }) 
+  $routeProvider
+    .when('/', {
+      templateUrl: 'ngview.tmpl.html',
+      controller: 'AppCtrl'
+    }) 
+    .when('/pizza', {
+      template: "yum" 
+    })
+    .otherwise({
+      template: "this doesn't exist"
+    })
 });
 
 app.controller('AppCtrl', function($scope) {
