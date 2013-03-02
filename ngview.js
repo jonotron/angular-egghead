29c65206b1a4f50c4e79abc3ab012bae1b4ecb8d
Jonathan Bowers <jonotron@gmail.com>
2013-03-02 14:21:45 -0800

    Promises

--- ngview.js
+++ ngview.js
@@ -20,7 +20,16 @@ app.config(function($routeProvider) {
     });
 });
 
-app.controller('AppCtrl', function($scope, $routeParams) {
+app.controller('AppCtrl', function($scope, $q) {
+  var defer = $q.defer();
+
+  defer.promise.then(function(weapon) {
+    alert('You can have my' + weapon);
+    return 'bow';
+  }).then(function(weapon) {
+    alert('And my' + weapon); 
+  });
+  defer.resolve("sword");
   $scope.model = {
     message: "This is my app!!" 
   }
