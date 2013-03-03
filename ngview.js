f2280b7f7f5998ea8d78fb11c9c38de446006ec9
Jonathan Bowers <jonotron@gmail.com>
2013-03-03 13:56:36 -0800

    Directive for errors

--- ngview.js
+++ ngview.js
@@ -14,10 +14,19 @@ app.config(function($routeProvider) {
     });
 });
 
+app.directive('error', function($rootScope) {
+  return {
+    restrict:'E',
+    template: '<div class="alert-box alert" ng-show="isError">Error!!!</div>',
+    link: function(scope) {
+      $rootScope.$on('$routeChangeError', function(event, current, previous, rejection) {
+        scope.isError = true;
+      });
+    }
+  }
+});
+
 app.controller('AppCtrl', function($rootScope) {
-  $rootScope.$on('$routeChangeError', function(event, current, previous, rejection) {
-    console.log(rejection); 
-  });
 });
 
 var viewCtrl = app.controller('ViewCtrl', function($scope, $route) {
